baeae9fdd7f1422f53c07156f299e0703a99c338
caiyuan <caiyuan_0111@163.com>
2019-07-31 00:24:33 +0800

    上传代码框架.

---
+++ taotao-parent/taotao-common/src/main/java/com/zcy/MyAnnotation.java
@@ -0,0 +1,14 @@
+package com.zcy;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Target;
+
+/**
+ * @Description:
+ * @Author: zhangcaiyuan
+ * @Date: Create in 0:46 2019/7/29
+ * @Modified By:
+ */
+@Target(ElementType.TYPE)
+public @interface MyAnnotation {
+}
